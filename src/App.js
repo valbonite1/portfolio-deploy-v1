18e1fd8cfcf73e7f20681f99ed54ecb5ab86b068
valbonite1 <68104824+valbonite1@users.noreply.github.com>
2020-11-02 00:04:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
+        <HeroSection />
         <Route render={({location}) => (
           <TransitionGroup>
           <CSSTransition
