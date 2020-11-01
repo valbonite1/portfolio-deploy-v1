53f15ee4e74c76e71366c0c423dffd70afa6bce2
valbonite1 <valbonite@gmail.com>
2020-11-01 22:50:56 +0800

    Updates

---
+++ precache-manifest.dd7acca9c35c3010dcd2499db2b1247f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac630121885deab2e88b1e9a575ff7c",
+    "url": "/portfolio-deploy-v1/index.html"
+  },
+  {
+    "revision": "a2ff56048ce73b484e47",
+    "url": "/portfolio-deploy-v1/static/css/main.53a973d1.chunk.css"
+  },
+  {
+    "revision": "4720b0570079585a08b2",
+    "url": "/portfolio-deploy-v1/static/js/2.57ab5675.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/portfolio-deploy-v1/static/js/2.57ab5675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ff56048ce73b484e47",
+    "url": "/portfolio-deploy-v1/static/js/main.f57f4385.chunk.js"
+  },
+  {
+    "revision": "f4168e68922bf97b7b61",
+    "url": "/portfolio-deploy-v1/static/js/runtime-main.32cf640d.js"
+  },
+  {
+    "revision": "ec1d212afa32b2f81b5721858291f179",
+    "url": "/portfolio-deploy-v1/static/media/BONITE.ec1d212a.png"
+  },
+  {
+    "revision": "2062d77da8946494fbd754dca0541745",
+    "url": "/portfolio-deploy-v1/static/media/white-logo.2062d77d.png"
+  }
+]);
